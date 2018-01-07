9a7b0785d15b82b4710eb301c1fe82757b9476a4
dejvids <dawidsurys@hotmail.com>
2018-01-07 20:20:25 +0100

    Building URL

--- app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -66,6 +69,16 @@ public final class NetworkUtils {
      */
     public static URL buildUrl(String locationQuery) {
         // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
+        Uri.Builder b = new Uri.Builder();
+        b.appendPath(FORECAST_BASE_URL);
+        b.appendQueryParameter(QUERY_PARAM,locationQuery);
+        String urlString = b.build().toString();
+
+        try {
+            URL url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
